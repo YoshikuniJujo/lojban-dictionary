64f1bb7819aee61df49261b531471f74114961bc
iocikun.jujon <PAF01143@nifty.ne.jp>
2012-07-24 08:46:05 +0900

    change icon and add images etc -> version 0.10.1

--- project/Build.scala
+++ project/Build.scala
@@ -6,8 +6,8 @@ import AndroidKeys._
 object General {
   val settings = Defaults.defaultSettings ++ Seq (
     name := "Lojban Dictionary",
-    version := "0.10",
-    versionCode := 16,
+    version := "0.10.1",
+    versionCode := 17,
     scalaVersion := "2.9.1",
     platformName in Android := "android-15"
   )
