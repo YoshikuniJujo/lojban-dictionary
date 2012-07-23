aac4c3b14e74341b18ecc96a40d0419741e7d3f7
iocikun.jujon <PAF01143@nifty.ne.jp>
2012-07-23 22:10:53 +0900

    add pure lojban dictionary

--- src/main/scala/ReadDictionary.scala
+++ src/main/scala/ReadDictionary.scala
@@ -9,19 +9,6 @@ import _root_.android.content.SharedPreferences
 class ReadDictionary(asset: AssetManager, sp: SharedPreferences) {
 	val initialString = "Hello!\nThis is Lojban dictionary!"
 
-	lazy val reader = new BufferedReader(new InputStreamReader(
-		asset.open("gismu.txt"), "UTF-8"));
-	lazy val cmavo = new BufferedReader(new InputStreamReader(
-		asset.open("cmavo.txt"), "UTF-8"));
-
-	var list: List[String] = List()
-	var clist: List[String] = List()
-	var line = ""
-
-	while ({line = reader.readLine(); line != null}) { list ::= line }
-	line = ""
-	while ({line = cmavo.readLine(); line != null}) { clist ::= line }
-
 	def getRafsi(rafsi: String): Node = {
 		val lang = if (sp.contains("lang")) sp.getString("lang", "en")
 			else "en"
@@ -191,45 +178,4 @@ class ReadDictionary(asset: AssetManager, sp: SharedPreferences) {
 		else rafsi + ":no result"
 	}
 
-	def lojToEn(loj: String): String = {
-		var line = ""
-		for (line <- list){
-			if (line.slice(1, 100).startsWith(loj)) {
-				return line
-			}
-		}
-		for (line <- clist) {
-			if (line.slice(1, 100).startsWith(loj)) {
-				return line
-			}
-		}
-		return loj + ": no result"
-	}
-
-	def enToLoj(en: String): String = {
-		var line = ""
-		for (line <- list) {
-			if (line.slice(20, 100).startsWith(en)) {
-				return line
-			}
-		}
-		for (line <- clist) {
-			if (line.slice(20, 100).startsWith(en)) {
-				return line
-			}
-		}
-		return en + ": no result"
-	}
-
-	def rafsiToLoj(rafsi: String): String = {
-		var line = ""
-		for (line <- list) {
-			if (line.slice(7, 10).startsWith(rafsi) ||
-				line.slice(11, 14).startsWith(rafsi) ||
-				line.slice(15, 19).startsWith(rafsi)) {
-				return line
-			}
-		}
-		return rafsi + ": no result"
-	}
 }
