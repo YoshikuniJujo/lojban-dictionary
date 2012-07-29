713f66cc4642277786efa4e380786b42615167e4
iocikun.jujon <PAF01143@nifty.ne.jp>
2012-07-30 01:03:21 +0900

    add android:launchMode=singleTask

--- project/Build.scala
+++ project/Build.scala
@@ -6,8 +6,8 @@ import AndroidKeys._
 object General {
   val settings = Defaults.defaultSettings ++ Seq (
     name := "Lojban Dictionary",
-    version := "0.15.1",
-    versionCode := 39,
+    version := "0.15.2",
+    versionCode := 40,
     scalaVersion := "2.9.1",
     platformName in Android := "android-15"
   )
