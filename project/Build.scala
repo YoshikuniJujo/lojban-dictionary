3adb638219eb60b034cdf7210b9516371e2a5d24
iocikun.jujon <PAF01143@nifty.ne.jp>
2012-08-03 05:45:23 +0900

    version to 0.15.6 (44)

--- project/Build.scala
+++ project/Build.scala
@@ -6,8 +6,8 @@ import AndroidKeys._
 object General {
   val settings = Defaults.defaultSettings ++ Seq (
     name := "Lojban Dictionary",
-    version := "0.15.5",
-    versionCode := 43,
+    version := "0.15.6",
+    versionCode := 44,
     scalaVersion := "2.9.1",
     platformName in Android := "android-15"
   )
