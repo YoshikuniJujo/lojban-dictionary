7cf77f9e2ae7c2b2e40b050c12527f3fcc419c31
iocikun.jujon <PAF01143@nifty.ne.jp>
2012-08-10 10:12:30 +0900

    add french and german -> version 0.16.3 (52)

--- project/Build.scala
+++ project/Build.scala
@@ -6,8 +6,8 @@ import AndroidKeys._
 object General {
   val settings = Defaults.defaultSettings ++ Seq (
     name := "Lojban Dictionary",
-    version := "0.16.2",
-    versionCode := 51,
+    version := "0.16.3",
+    versionCode := 52,
     scalaVersion := "2.9.1",
     platformName in Android := "android-15"
   )
