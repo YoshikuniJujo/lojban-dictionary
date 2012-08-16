b80882259888660841f9f2c9635ffa85b4eff6c0
iocikun.jujon <PAF01143@nifty.ne.jp>
2012-08-16 21:57:31 +0900

    enable scrolling when search from Quick Search Box -> version 0.18.2

--- project/Build.scala
+++ project/Build.scala
@@ -6,8 +6,8 @@ import AndroidKeys._
 object General {
   val settings = Defaults.defaultSettings ++ Seq (
     name := "Lojban Dictionary",
-    version := "0.18.1",
-    versionCode := 57,
+    version := "0.18.2",
+    versionCode := 58,
     scalaVersion := "2.9.1",
     platformName in Android := "android-15"
   )
