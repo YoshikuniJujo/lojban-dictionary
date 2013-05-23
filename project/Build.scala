e5ff98a1be90732ee1a28419f890d74b3f0408f0
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2013-05-23 10:56:49 +0900

    update all language's database

--- project/Build.scala
+++ project/Build.scala
@@ -6,8 +6,8 @@ import AndroidKeys._
 object General {
   val settings = Defaults.defaultSettings ++ Seq (
     name := "Lojban Dictionary",
-    version := "0.18.9",
-    versionCode := 65,
+    version := "0.18.10",
+    versionCode := 66,
     scalaVersion := "2.9.1",
     platformName in Android := "android-17"
   )
