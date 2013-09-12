d2b4565d6a10b0810e9491e10dba7df86b5b831d
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2013-09-12 16:56:06 +0900

    reverse to no money

--- project/Build.scala
+++ project/Build.scala
@@ -6,8 +6,8 @@ import AndroidKeys._
 object General {
   val settings = Defaults.defaultSettings ++ Seq (
     name := "Lojban Dictionary",
-    version := "0.18.22",
-    versionCode := 78,
+    version := "0.18.26",
+    versionCode := 82,
     scalaVersion := "2.9.1",
     platformName in Android := "android-17"
   )
