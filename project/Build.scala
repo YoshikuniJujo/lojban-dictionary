09dbf2ce98a65bc529c057009e44b92382767fec
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2013-11-11 16:00:52 +0900

    update dictionary data

--- project/Build.scala
+++ project/Build.scala
@@ -6,8 +6,8 @@ import AndroidKeys._
 object General {
   val settings = Defaults.defaultSettings ++ Seq (
     name := "Lojban Dictionary",
-    version := "0.18.26",
-    versionCode := 82,
+    version := "0.18.28",
+    versionCode := 84,
     scalaVersion := "2.9.1",
     platformName in Android := "android-17"
   )
